00af686b21e27ce1a2c2e206841cd45ac1187693
aljahmi20002-collab <aljahmi20002@gmail.com>
2026-05-31 19:43:34 +0300

    Add files via upload

---
+++ legal/legal/doctype/plea_or_guilty_or_trial_criminal/plea_or_guilty_or_trial_criminal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Plea or Guilty or Trial Criminal', {
+	// refresh: function(frm) {
+
+	// }
+});
