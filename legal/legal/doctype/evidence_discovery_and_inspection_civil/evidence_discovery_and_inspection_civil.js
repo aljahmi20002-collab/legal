ac8515ac15837bcb787cedf607cd1ab315ccaa79
aljahmi20002-collab <aljahmi20002@gmail.com>
2026-05-31 19:40:38 +0300

    Add files via upload

---
+++ legal/legal/doctype/evidence_discovery_and_inspection_civil/evidence_discovery_and_inspection_civil.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Evidence Discovery and Inspection Civil', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ legal/legal/doctype/filing_of_charge_sheet_criminal/filing_of_charge_sheet_criminal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Filing of Charge Sheet Criminal', {
+	// refresh: function(frm) {
+
+	// }
+});
