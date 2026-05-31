2720768b5c3cda6e5e5e4fcd806b99f5a4d245cd
aljahmi20002-collab <aljahmi20002@gmail.com>
2026-05-31 19:37:19 +0300

    Add files via upload

---
+++ legal/legal/doctype/advocate_master/advocate_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Advocate Master', {
+	// refresh: function(frm) {
+
+	// }
+});
